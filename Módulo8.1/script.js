4b05c7ef9a4fe808202c42b149bc5db018b849db
João Victor <vitor_golden06@outlook.com>
2021-04-28 18:29:36 -0400

    Update script.js

--- Módulo8.1/script.js
+++ Módulo8.1/script.js
@@ -11,7 +11,57 @@
 // - Subtração
 // * Multiplicação
 // / Divisão
-// % Módulo
+// ** Potenciação
+// % Módulo (resto da divisão)
 // ++ Incremento
 // -- Decremento
 
+//Atribuições
+
+//  =   x = y   x = y
+// +=   x += y  x = x + y
+// -=   x -= y  x = x - y
+// *=   x *= y  x = x * y
+// /=   x /= y  x = x / y
+// %=   x %= y  x = x % y
+
+// Operadores comparativos
+
+// == igual a
+// === valor igual e igual ( considera o tipo da variável )
+// ! = não é igual
+// !== não igual ou não igual ( considera o tipo da variável ) 
+// > maior que 
+// < menor que 
+// > = maior que ou igual a
+// < = menor ou igual a 
+
+// Operadores lógicos
+
+// && e 
+// || ou
+// Negativo (!)
+
+//Exemplos ():
+
+// var idade = 20;
+
+// var menor25 = idade <= 25;
+// var maior15 = idade >= 15;
+// var entre = menor25 && maior15; 
+
+// console.log("Idade: ", idade);
+// console.log("Idade menor que 25", menor25);
+// console.log("Idade maior que 15", maior15);
+// console.log("Entre 15 e 25 ", entre);
+
+// var idade = 9;
+
+// var menor10 = idade <= 10;
+// var maior65 = idade >= 65;
+// var gratuito = menor10 || maior65; 
+
+// console.log("Idade: ", idade);
+// console.log("Idade menor que 10", menor10);
+// console.log("Idade maior que 65", maior65);
+// console.log("Tem direito a gratuidade", gratuito);
