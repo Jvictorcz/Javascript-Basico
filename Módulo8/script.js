af6f0b08efd9e25803f69b2a909a7f16b951f4b8
João Victor <vitor_golden06@outlook.com>
2021-04-30 15:54:55 -0400

    Update script.js

--- Módulo8/script.js
+++ Módulo8/script.js
@@ -139,43 +139,66 @@
 
 // Condicionais - Switch
 
-var nota1 = 8.0;
-var nota2 = 8.0;
+// var nota1 = 8.0;
+// var nota2 = 8.0;
 
-var media = (nota1 + nota2 ) / 2;
+// var media = (nota1 + nota2 ) / 2;
 
-if(media >= 8){
+// if(media >= 8){
     
-    conceito = "Ótimo"
-}
-else if( media >= 6.5) {
+//     conceito = "Ótimo"
+// }
+// else if( media >= 6.5) {
   
-    conceito = "Bom"
-}else{
+//     conceito = "Bom"
+// }else{
     
-    conceito = "Regular"
+//     conceito = "Regular"
+// }
+
+// conceito = "Mais ou menos";
+
+// console.log(media);
+// console.log(conceito);
+
+// switch(conceito){
+
+//     case "Ótimo":
+//         console.log("Parábens, você é um ótimo aluno");
+//         break;
+//     case "Bom":
+//         console.log("Você está quase perfeito");
+//         break; 
+//     case "Regular":
+//         console.log("Estude mais um pouco")
+//         break;    
+//     default:
+//         console.log("Houve algum erro");
+//         break;
+
+
+// }
+
+
+// Repetição - for e while
+
+
+// var numero = 5;
+
+// for(var i = 0 ; i < numero ; i++  ){
+
+//     console.log("Executando o for , pela " + i + " vez" );
+// }
+
+// console.log("Acabou");
+
+
+var numero = -5;
+
+while(numero<0){
+
+    console.log("Número "+ numero);
+    numero++;
 }
 
-conceito = "Mais ou menos";
-
-console.log(media);
-console.log(conceito);
-
-switch(conceito){
-
-    case "Ótimo":
-        console.log("Parábens, você é um ótimo aluno");
-        break;
-    case "Bom":
-        console.log("Você está quase perfeito");
-        break; 
-    case "Regular":
-        console.log("Estude mais um pouco")
-        break;    
-    default:
-        console.log("Houve algum erro");
-        break;
-
-
-}
-
+console.log("Acabou");
